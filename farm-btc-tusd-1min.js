211f877ce1470aa4890236590ca259b00290d77e
www0z0k <www0z.step@gmail.com>
2020-03-25 13:27:26 +0300

    new data +new set

---
+++ farm-btc-tusd-1min.js
@@ -0,0 +1,2 @@
+const Farmer = require('./data-farmer').Farmer;
+const farm1min = new Farmer('btc-tusd-1minute-3001', 1, 3001, 'BTCTUSD', 500, 1000);
--- farm-test.js
+++ farm-test.js
@@ -1,2 +1,2 @@
 const Farmer = require('./data-farmer').Farmer;
-const farm1min = new Farmer('1minute3000port', 1, 3000);
+const farm1min = new Farmer('1minute3000port', 1, 3000, 'BTCUSDT', 500, 1000);
